7db81847cdb439c57ae050aff8040ca0714b5a34
dvenkatesan-tmx <dharshan.venkatesan@tmx.com>
2024-12-03 12:43:45 -0500

    Update spark1.py

--- scripts/pyspark/spark1.py
+++ scripts/pyspark/spark1.py
@@ -1,2 +1,4 @@
-import pytest
-from airflow.models import DagBag
+# sample_script.py
+def process_data():
+    with open('/tmp/output.txt', 'w') as f:
+        f.write("This is the output of the script.")
